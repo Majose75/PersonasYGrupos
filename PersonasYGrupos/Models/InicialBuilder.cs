3f2263de00d674848bd97b310c4044a3c6199385
Majose <mariajose.paesa@gmail.com>
2024-05-31 11:25:08 +0200

    Agregar archivos de proyecto.

---
+++ PersonasYGrupos/Models/InicialBuilder.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonasYGrupos.Models
+{
+    internal class InicialBuilder : IListaBuilder
+    {
+        public List<Employee> dameEmpleados()
+        {
+            List<Employee> empList = new List<Employee>();
+            empList.Add(new Employee()
+            {
+                ID = 1,
+                FName = "John",
+                MName = "",
+                LName = "Shields",
+                DOB = DateTime.Parse("1971/12/11"),
+                Sex = 'M'
+            });
+            empList.Add(new Employee()
+            {
+                ID = 2,
+                FName = "Mary",
+                MName = "Matthew",
+                LName = "Jacobs",
+                DOB = DateTime.Parse("1961/01/17"),
+                Sex = 'F'
+            });
+            empList.Add(new Employee()
+            {
+                ID = 3,
+                FName = "Amber",
+                MName = "Carl",
+                LName = "Agar",
+                DOB = DateTime.Parse("1971/12/23"),
+                Sex = 'M'
+            });
+            empList.Add(new Employee()
+            {
+                ID = 4,
+                FName = "Kathy",
+                MName = "",
+                LName = "Berry",
+                DOB = DateTime.Parse("1976/11/15"),
+                Sex = 'F'
+            });
+            empList.Add(new Employee()
+            {
+                ID = 5,
+                FName = "Lena",
+                MName = "Ashco",
+                LName = "Bilton",
+                DOB = DateTime.Parse("1978/05/11"),
+                Sex = 'F'
+            });
+            empList.Add(new Employee()
+            {
+                ID = 6,
+                FName = "Susanne",
+                MName = "",
+                LName = "Buck",
+                DOB = DateTime.Parse("1965/03/7"),
+                Sex = 'F'
+            });
+            empList.Add(new Employee()
+            {
+                ID = 7,
+                FName = "Jim",
+                MName = "",
+                LName = "Brown",
+                DOB = DateTime.Parse("1972/09/11"),
+                Sex = 'M'
+            });
+            empList.Add(new Employee()
+            {
+                ID = 8,
+                FName = "Jane",
+                MName = "G",
+                LName = "Hooks",
+                DOB = DateTime.Parse("1972/12/11"),
+                Sex = 'F'
+            });
+            empList.Add(new Employee()
+            {
+                ID = 9,
+                FName = "Robert",
+                MName = "",
+                LName = "",
+                DOB = DateTime.Parse("1964/06/28"),
+                Sex = 'M'
+            });
+            empList.Add(new Employee()
+            {
+                ID = 10,
+                FName = "Cindy",
+                MName = "Preston",
+                LName = "Fox",
+                DOB = DateTime.Parse("1978/01/11"),
+                Sex = 'M'
+            });
+            return empList;
+        }
+    }
+}
---
+++ PersonasYGrupos/Program.cs
@@ -0,0 +1,78 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Data.Common;
+using System.Xml.Schema;
+using PersonasYGrupos.Models;
+
+IListaBuilder MiGenerador = new InicialBuilder();
+var elementos = MiGenerador.dameEmpleados();
+
+
+//Mostrar todos los empleados que son chicos.
+Console.WriteLine("\nMostrar todos los empleados que son chicos");
+var Consulta1 =
+    from item in elementos
+        where item.Sex == 'M'
+        select item;
+foreach (var texto in Consulta1)
+{
+    Console.WriteLine(texto.FName + " es: "+ texto.Sex);
+}
+
+//Mostrar todos los empleados que tienen más de 16 años.
+Console.WriteLine("\nMostrar todos los empleados que tienen más de 16 años");
+var Consulta2= from item in elementos where (DateTime.Today).Year - (item.DOB).Year > 16 select item;
+
+foreach (var texto in Consulta2)
+{
+    Console.WriteLine(texto.FName + " tiene + de 16 años (" + (texto.DOB).Year + ")");
+}
+
+//Mostrar todos los empleados que son chicas, ordenado por FName.
+Console.WriteLine("\nMostrar todos los empleados que que son chicas, ordenado por nombre:");
+
+var Consulta3 = elementos.Where(item => item.Sex == 'F').OrderBy(item => item.FName);
+
+foreach (var texto in Consulta3)
+{
+    Console.WriteLine(texto.FName + " es: " + (texto.Sex));
+}
+
+//Mostrar los empleados cuyo nombre comience por j y sea chico, ordenar por edad
+Console.WriteLine("\nMostrar todos los empleados cuyo nombre comience por J y sea chico, ordenado por edad:");
+var Consulta4 = 
+    from item in elementos
+    where item.FName.StartsWith("J") && item.Sex=='M'
+    orderby item.DOB
+    select item;
+
+foreach (var texto in Consulta4)
+{
+    Console.WriteLine(texto.FName + " es: " + (texto.Sex)+" y su año de Nacimiento es: " + (texto.DOB).Year);
+}
+
+//Sacar los empleados agrupados por Sex.
+Console.WriteLine("\nMostrar todos los empleados agrupados por sexo:");
+var Consulta5 = elementos.GroupBy(item => item.Sex);
+
+foreach (var texto in Consulta5)
+{
+    Console.WriteLine("\nSexo "+ texto.Key + ":");
+    foreach (var texto2 in texto)
+    {
+        Console.Write(texto2.FName + " ");
+    }
+}
+
+//Sacar los empleados ordenados por FName y Agrupados por Sex.
+Console.WriteLine("\nMostrar todos los empleados ordenados por nombrey agrupados por sexo:");
+var Consulta6 = elementos.OrderBy(item=>item.FName).GroupBy(item=>item.Sex);
+
+foreach (var texto in Consulta6)
+{
+    Console.WriteLine("\nSexo " + texto.Key + ":");
+    foreach (var texto2 in texto)
+    {
+        Console.Write(texto2.FName + " ");
+    }
+}
